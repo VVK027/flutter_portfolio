243d8f7190e7013ead222bdeadf6df6fd3f35bcb
vivek <vickyvivek2727@gmail.com>
2025-09-23 20:02:27 +0530

    CI/CD Pipeline, Added Github actions

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,12 +1,11 @@
 import 'package:go_router/go_router.dart';
 import 'package:vivek_portfolio/presentation/portfolio/screen/portfolio_page.dart';
 
-
 final appRouter = GoRouter(
   routes: [
     GoRoute(
       path: '/',
-      builder: (_, __) => const PortfolioPage(),
+      builder: (_, _) => const PortfolioPage(),
       routes: [
         GoRoute(
           path: 'section/:id',
